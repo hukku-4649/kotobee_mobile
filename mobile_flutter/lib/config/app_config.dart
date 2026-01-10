557a69f61310eb90bc51e344685e0e18683e2ca7
hukku-4649 <121495610+hukku-4649@users.noreply.github.com>
2026-01-10 13:21:17 +0800

    Update app_config.dart

--- mobile_flutter/lib/config/app_config.dart
+++ mobile_flutter/lib/config/app_config.dart
@@ -4,8 +4,6 @@ class AppConfig {
         defaultValue: 'http://127.0.0.1:8000',
         // defaultValue: 'https://127.0.0.1:443',
         // defaultValue: 'https://192.0.0.3:8000',
-        // defaultValue: 'https://willyard-lashaunda-conformable.ngrok-free.dev',
         // defaultValue: 'https://172.20.10.3',
-        // defaultValue: 'https://10.20.61.136', 
     );
 }
